d337c2af75a10351890ad9a71ae4fa5a6821fc53
rhanc <146234470+rhanc@users.noreply.github.com>
2024-02-22 17:17:18 -0500

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_5
+{
+    internal class Person
+    {
+        private int id_;
+        private double mass_;
+        private string name_;
+        private string gender_;
+        private int height_;
+        private string human_;
+        private string eye_color_;
+        private string homeworld_;
+        private string birth_year_;
+        private string hair_color_;
+        private string skin_color_;
+
+        public Person(int id_, double mass_, string name_, string gender_, int height_, string human_, string eye_color_, string homeworld_, string birth_year_, string hair_color_, string skin_color_)
+        {
+            this.id_ = id_;
+            this.mass_ = mass_;
+            this.name_ = name_;
+            this.gender_ = gender_;
+            this.height_ = height_;
+            this.human_ = human_;
+            this.eye_color_ = eye_color_;
+            this.homeworld_ = homeworld_;
+            this.birth_year_ = birth_year_;
+            this.hair_color_ = hair_color_;
+            this.skin_color_ = skin_color_;
+        }
+
+        public int Id { get => id_; set => id_ = value; }
+        public double Mass { get => mass_; set => mass_ = value; }
+        public string Name { get => name_; set => name_ = value; }
+        public string Gender { get => gender_; set => gender_ = value; }
+        public int Height { get => height_; set => height_ = value; }
+        public string Human { get => human_; set => human_ = value; }
+        public string Eye_color { get => eye_color_; set => eye_color_ = value; }
+        public string Homeworld { get => homeworld_; set => homeworld_ = value; }
+        public string Birth_year { get => birth_year_; set => birth_year_ = value; }
+        public string Hair_color { get => hair_color_; set => hair_color_ = value; }
+        public string Skin_color { get => skin_color_; set => skin_color_ = value; }
+
+        public override string ToString()
+        {
+            string str = "ID: "+Id+"\nMass: "+Mass+"\nName: "+Name+"\nGender: "+Gender+"\nHeight: "+Height;
+            str += "\nHuman: " + Human + "\nEye Color: " + Eye_color + "\nHomeword: " + Homeworld;
+            str += "\nBirth Year: " + Birth_year + "\nHair Color: " + Hair_color + "\nSkin Color: " + Skin_color;
+            return str+"\n";
+        }
+
+        public bool IsEqual(string value, string field)
+        {
+            value = value.ToLower();
+            bool result = false;
+            if (Id.ToString() == value && field == "id")
+            {
+                result = true;
+            }
+            else if (Mass.ToString() == value && field == "mass")
+            {
+                result = true;
+            }
+            else if (Name.ToLower().Contains(value) && field == "name")
+            {
+                result = true;
+            }
+            else if (Gender.ToLower().Replace('"',' ').Trim() == (value) && field == "gender")
+            {
+                result = true;
+            }
+            else if (Height.ToString() == value && field == "height")
+            {
+                result = true;
+            }
+            else if (Human.ToLower().Contains(value) && field == "human")
+            {
+                result = true;
+            }
+            else if (Eye_color.ToLower().Contains(value) && field == "eye_color")
+            {
+                result = true;
+            }
+            else if (Homeworld.ToLower().Contains(value) && field == "homeworld")
+            {
+                result = true;
+            }
+            else if (Birth_year.ToLower().Contains(value) && field == "birth_year")
+            {
+                result = true;
+            }
+            else if (Hair_color.ToLower().Contains(value) && field == "hair_color")
+            {
+                result = true;
+            }
+
+            else if (Skin_color.ToLower().Contains(value) && field == "skin_color")
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using Lab4_5;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Linq;
+
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://star-wars-characters.p.rapidapi.com/46DYBV/star_wars_characters"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", "051606a134msh4b860df37205ba3p1cf21fjsn14affe616766" },
+        { "X-RapidAPI-Host", "star-wars-characters.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    JsonNode data = JsonObject.Parse(body);
+
+    List<Person> arr = GetData(data);
+    string option = ""; 
+    while (option.ToLower() != "q")
+    {
+        Console.WriteLine("1. ID");
+        Console.WriteLine("2. Name");
+        Console.WriteLine("3. Gender"); 
+        Console.WriteLine("4. Eye Color");
+        Console.WriteLine("5. Homeworld");
+        Console.WriteLine("6. Birth Year");
+        Console.WriteLine("7. Hair Color");
+        Console.WriteLine("8. Skin Color");
+        Console.WriteLine("Q: Close");
+        option = Console.ReadLine();
+        if (option.ToLower() != "q")
+        {
+            string field = "";
+            switch (option)
+            {
+                case "1":
+                    field = "id";
+                    Console.WriteLine("Type the ID you want to search for.");
+                    break;
+                case "2":
+                    field = "name";
+                    Console.WriteLine("Type the Name you want to search for.");
+                    break;
+                case "3":
+                    field = "gender";
+                    Console.WriteLine("Type the Gender you want to search for.");
+                    break;
+                case "4":
+                    field = "eye_color";
+                    Console.WriteLine("Type the Eye Color you want to search for.");
+                    break;
+                case "5":
+                    field = "homeworld";
+                    Console.WriteLine("Type the Homeworld you want to search for.");
+                    break;
+                case "6":
+                    field = "birth_year";
+                    Console.WriteLine("Type the Birth Year you want to search for.");
+                    break;
+                case "7":
+                    field = "hair_color";
+                    Console.WriteLine("Type the Hair Color you want to search for.");
+                    break;
+                case "8":
+                    field = "skin_color";
+                    Console.WriteLine("Type the Skin Color you want to search for.");
+                    break;
+            }
+            option = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i].IsEqual(option, field) == true)
+                {
+                    count++;
+                    Console.WriteLine(arr[i].ToString());
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("No Data Found");
+            }    
+        }
+    }
+}
+List<Person> GetData(JsonNode data)
+{
+    List<Person> people = new List<Person>();
+    for (int i = 0; i < data.AsArray().Count; i++)
+    {
+        try
+        {
+            string value = data[i].ToString().Replace("{", "");
+            value = value.Replace("}", "");
+            string[] arr = value.Split(','); 
+            int id = int.Parse(arr[0].Split(":")[1]); 
+            double mass = 0;
+            if (arr[1].Contains("NA") == false && arr[1].Contains("null") == false)
+            {
+                string val = arr[1].Split(":")[1];
+                val = (val.Replace('"', ' '));
+                mass = double.Parse(val.Trim());
+            }
+
+            string name = arr[2].Split(":")[1];
+            string gender = arr[3].Split(":")[1];
+            int height = int.Parse(arr[4].Split(":")[1].Substring(2, 3).Trim('"'));
+            string species = GetValue(arr[5]); 
+            string eye_color = GetValue(arr[6]);
+            string homeworld = GetValue(arr[7]);
+            string birth_year = GetValue(arr[8]);
+            string hair_color = GetValue(arr[9]);
+            string skin_color = GetValue(arr[10]);
+
+            Person p = new Person(id, mass, name, gender, height, species, eye_color, homeworld, birth_year, hair_color, skin_color);
+            people.Add(p);
+        }
+        catch (Exception e)
+        {
+            //Console.WriteLine(e.Message);
+        }
+    }
+
+    //people.ForEach(p => Console.WriteLine(p.ToString()));
+    return people;
+}
+
+string GetValue(string str)
+{
+    try
+    {
+        return str.Split(":")[1];
+    }
+    catch (Exception e)
+    {
+        return "";
+    }
+}
